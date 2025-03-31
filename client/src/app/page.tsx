0dbac0b23fcf5954ba3a8e5ca1713ebd1d5360f4
CristopherAguilar10 <jankaguilar1030@gmail.com>
2025-03-31 14:38:45 -0600

    feat:removing things

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,13 @@
-import PoolsPage from "./Pools/page";
+import Footer from "../components/footer";
+import Header from "../components/header/page";
+import WelcomePage from "./LandingPage/WelcomePage";
 
 export default function HomePage() {
-	return <PoolsPage />;
+	return (
+		<>
+			<Header />
+			<WelcomePage />
+			<Footer />
+		</>
+	);
 }
