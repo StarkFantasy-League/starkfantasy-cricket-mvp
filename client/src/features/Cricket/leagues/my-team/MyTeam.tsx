0dd534b394cc184cb94d58fc3550fa269ffc2703
Dario Zamora Rojas <dariozamora0731@gmail.com>
2025-04-08 12:48:38 -0600

    feat: add all routes

--- client/src/features/Cricket/leagues/my-team/MyTeam.tsx
+++ client/src/features/Cricket/leagues/my-team/MyTeam.tsx
@@ -31,7 +31,10 @@ const MyTeam = () => {
             isSidebarOpen ? "w-[250px]" : "w-0"
           } h-[calc(100vh-100px)] transition-all duration-300 ease-in-out overflow-hidden`}
         >
-          <Sidebar />
+      <Sidebar
+        currentImage="/assets/images/indianLeague.png"
+        className="fixed left-0 top-[100px] h-full w-64 bg-[#1F1B2C] z-[1000]"
+      />
         </div>
 
         <div
