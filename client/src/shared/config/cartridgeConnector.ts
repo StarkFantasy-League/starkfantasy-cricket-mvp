61204630cf7c52cec42fec8fef8e988d5f8cd45e
Luis Jiménez <87153882+jimenezz22@users.noreply.github.com>
2025-04-19 12:42:46 -0600

    [FEAT] Prepare repo for Dojo Integration with the Client  (#51)
    * refactor: remove unused files and add new corresponding files
    
    * feat: model.gen.ts bindings
    
    * feat: add system functions in contracts.gen.ts
    
    * feat: update contract permissions in cartridgeConnector
    
    * feat: implement custom hook to retrieve user data based on account
    
    * feat: add custom hook to retrieve user's bet for a specific pool
    
    * feat: implement useSystemCalls hook for user interactions and betting functionality
    
    * feat: add utility functions
    
    * feat: implement Zustand store for application state management
    
    * fix: update import path for dojoConfig in main.tsx
    
    * feat: add functions to fetch data  from GraphQL API

--- client/src/shared/config/cartridgeConnector.ts
+++ client/src/shared/config/cartridgeConnector.ts
@@ -8,21 +8,23 @@ const theme = "";
 // Define your contract permissions
 const policies: SessionPolicies = {
   contracts: {
-    ['0x5b46553782ae5c36b9958cedba3fdfe63572f19183726ae8db95323a61b10cd']: {
-      methods: [
-        // List all methods your frontend needs to access
-        {
-          name: "method1",
-          entrypoint: "method1"
-        },
-        {
-          name: "method2",
-          entrypoint: "method2"
-        },
-        // Add all your contract methods
-      ],
-    },
-    // Add more contracts if needed
+      ['0x5b46553782ae5c36b9958cedba3fdfe63572f19183726ae8db95323a61b10cd']: {
+          methods: [
+              { name: "place_match_bet", entrypoint: "place_match_bet" },
+              { name: "place_special_bet", entrypoint: "place_special_bet" },
+              { name: "claim_reward", entrypoint: "claim_reward" },
+              { name: "get_user_bet", entrypoint: "get_user_bet" },
+              { name: "has_bet_on_pool", entrypoint: "has_bet_on_pool" },
+              { name: "is_bet_claimable", entrypoint: "is_bet_claimable" },
+          ],
+      },
+      ['0x23f5eafef6a4e44b4c960d2de35ff2e65929509658d1ca645ca94bbcce112fd']: {
+          methods: [
+              { name: "spawn_user", entrypoint: "spawn_user" },
+              { name: "get_user_data", entrypoint: "get_user_data" },
+              { name: "get_points_balance", entrypoint: "get_points_balance" },
+          ],
+      },
   },
 }
 
