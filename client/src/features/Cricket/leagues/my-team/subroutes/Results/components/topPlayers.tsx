e61d474289236cd18f7d9515cf410ac89c44fcd2
Brenda Mareco <107716199+brendaamareco@users.noreply.github.com>
2025-04-27 23:09:18 -0300

    Feat: Create Results Page (#65)
    * Added result stats
    
    * Added recent matches and refactored results
    
    * Created card section component
    
    * Added top players section. Extracted data into a new file
    
    * Deleted duplicated Team Info
    
    * Added new icons
    
    * Added chart component and weekly progress section
    
    * Adjusted result page styles
    
    * Added chip icon
    
    * Added pool results section
    
    * Adjusted results page
    
    * Updated colors and adjusted results page
    
    * Adjusted pool results and weekly progress section
    
    * Fix map key issue
    
    * Reorganized folders. Set up alias for asset import

---
+++ client/src/features/Cricket/leagues/my-team/subroutes/Results/components/topPlayers.tsx
@@ -0,0 +1,22 @@
+import { Player } from "../../../../../../../shared/data/resultsData";
+import TopPlayer from "./topPlayer";
+import trophy from "@assets/icons/trophy.svg";
+import { CardSection } from "./cardSection";
+
+interface TopPlayersProps {
+    players: Player[];
+}
+
+const TopPlayers: React.FC<TopPlayersProps> = ({ players }) => {
+    return (
+        <CardSection title="Top Fantasy Players" iconSrc={trophy}>
+            <div className="flex flex-col space-y-1 w-100 justify-stretch">
+                {players.map((player, index) => (
+                    <TopPlayer key={index} {...player} />
+                ))}
+            </div>
+        </CardSection>
+    );
+};
+
+export default TopPlayers;
